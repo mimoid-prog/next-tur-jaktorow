143fbe184d3498e5cc678d47ec6ca68a68a0f145
mimoid-prog <mimoid.prog@gmail.com>
2020-10-30 13:40:48 +0100

    Dodano google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,30 @@
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import "../styles/globals.scss";
 import Router from "next/router";
 import NProgress from "nprogress";
 import "styles/select.scss";
 import "nprogress/nprogress.css";
+import { useRouter } from "next/router";
+import * as gtag from "gtag";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
