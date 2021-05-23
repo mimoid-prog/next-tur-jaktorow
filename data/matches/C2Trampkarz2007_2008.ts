7769462a399fcdd31ba6d75bb7390d949db78271
mimoid-prog <mimoid.prog@gmail.com>
2021-05-23 11:14:41 +0200

    Update

--- data/matches/C2Trampkarz2007_2008.ts
+++ data/matches/C2Trampkarz2007_2008.ts
@@ -1,25 +1,25 @@
 export default {
   matchOne: {
     title: "Poprzedni mecz",
-    logos: ["ksiazenice.jpg", "tur.jpg"],
-    score: "0  :  4",
-    clubs: ["UKS OFFensywa Książenice", "LKS Tur Jaktorów"],
+    logos: ["ks-raszyn.jpg", "tur.jpg"],
+    score: "4  :  3",
+    clubs: ["KS Raszyn", "LKS Tur Jaktorów"],
     isText: false,
-    date: "08-05-2021",
-    time: "12:45",
-    home: "(u nas)",
+    date: "21-05-2021",
+    time: "17:00",
+    home: "(wyjazd)",
     paused: false,
     canceled: false
   },
   matchTwo: {
     title: "Następny mecz",
-    logos: ["uks-bss-irzyk.jpg", "tur.jpg"],
+    logos: ["tur.jpg", "glks-nadarzyn.jpg"],
     score: "wkrótce",
-    clubs: ["UKS BSS Irzyk", "LKS Tur Jaktorów"],
+    clubs: ["LKS Tur Jaktorów", "GLKS Nadarzyn"],
     isText: true,
-    date: "16-05-2021",
-    time: "14:30",
-    home: "(wyjazd)",
+    date: "25-05-2021",
+    time: "17:00",
+    home: "(u nas)",
     paused: false,
     canceled: false
   }
--- data/matches/D2Mlodzik2009.ts
+++ data/matches/D2Mlodzik2009.ts
@@ -1,25 +1,25 @@
 export default {
   matchOne: {
     title: "Poprzedni mecz",
-    logos: ["tur.jpg", "relax-radziwillow.jpg"],
-    score: "9  :  0",
-    clubs: ["LKS Tur Jaktorów", "GLKS Relax Radziwiłłów"],
+    logos: ["ozarowianka.jpg", "tur.jpg"],
+    score: "1  :  3",
+    clubs: ["KS Ożarowianka", "LKS Tur Jaktorów"],
     isText: false,
-    date: "09-05-2021",
-    time: "14:00",
-    home: "(u nas)",
+    date: "16-05-2021",
+    time: "19:00",
+    home: "(wyjazd)",
     paused: false,
     canceled: false
   },
   matchTwo: {
     title: "Następny mecz",
-    logos: ["ozarowianka.jpg", "tur.jpg"],
+    logos: ["tur.jpg", "teresin.jpg"],
     score: "wkrótce",
-    clubs: ["KS Ożarowianka", "LKS Tur Jaktorów"],
+    clubs: ["LKS Tur Jaktorów", "KS Teresin"],
     isText: true,
-    date: "16-05-2021",
-    time: "19:00",
-    home: "(wyjazd)",
+    date: "23-05-2021",
+    time: "12:00",
+    home: "(u nas)",
     paused: false,
     canceled: false
   }
