25d54f1e526532fe144662f1cafb8ddf77dce57a
mimoid-prog <mimoid.prog@gmail.com>
2021-05-30 16:55:55 +0200

    Update

--- data/matches/C2Trampkarz2007_2008.ts
+++ data/matches/C2Trampkarz2007_2008.ts
@@ -1,24 +1,24 @@
 export default {
   matchOne: {
     title: "Poprzedni mecz",
-    logos: ["ks-raszyn.jpg", "tur.jpg"],
-    score: "4  :  3",
-    clubs: ["KS Raszyn", "LKS Tur Jaktorów"],
+    logos: ["stare-babice.jpg", "tur.jpg"],
+    score: "2  :  3",
+    clubs: ["GKS Naprzód Stare Babice", "LKS Tur Jaktorów"],
     isText: false,
-    date: "21-05-2021",
-    time: "17:00",
+    date: "29-05-2021",
+    time: "11:00",
     home: "(wyjazd)",
     paused: false,
     canceled: false
   },
   matchTwo: {
     title: "Następny mecz",
-    logos: ["tur.jpg", "glks-nadarzyn.jpg"],
+    logos: ["tur.jpg", "znicz-pruszkow.jpg"],
     score: "wkrótce",
-    clubs: ["LKS Tur Jaktorów", "GLKS Nadarzyn"],
+    clubs: ["LKS Tur Jaktorów", "MKS Znicz II Pruszków"],
     isText: true,
-    date: "25-05-2021",
-    time: "17:00",
+    date: "05-06-2021",
+    time: "13:30",
     home: "(u nas)",
     paused: false,
     canceled: false
--- data/matches/senior.ts
+++ data/matches/senior.ts
@@ -1,25 +1,25 @@
 export default {
   matchOne: {
     title: "Poprzedni mecz",
-    logos: ["tur.jpg", "chlebnia.jpg"],
-    score: "1  :  4",
-    clubs: ["LKS Tur Jaktorów", "LKS Chlebnia II"],
+    logos: ["zaborow.jpg", "tur.jpg"],
+    score: "4  :  1",
+    clubs: ["KS Zaborowianka", "LKS Tur Jaktorów"],
     isText: false,
-    date: "22-05-2021",
-    time: "11:00",
-    home: "(u nas)",
+    date: "29-05-2021",
+    time: "15:00",
+    home: "(wyjazd)",
     paused: false,
     canceled: false
   },
   matchTwo: {
     title: "Następny mecz",
-    logos: ["zaborow.jpg", "tur.jpg"],
+    logos: ["tur.jpg", "promyk.jpg"],
     score: "wkrótce",
-    clubs: ["KS Zaborowianka", "LKS Tur Jaktorów"],
+    clubs: ["LKS Tur Jaktorów", "KS Promyk Nowa Sucha"],
     isText: true,
-    date: "29-05-2021",
-    time: "15:00",
-    home: "(wyjazd)",
+    date: "05-06-2021",
+    time: "10:00",
+    home: "(u nas)",
     paused: false,
     canceled: false
   }
